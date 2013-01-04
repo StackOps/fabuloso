2e35b6fbc1b820228e5d25a2a091f7f420192857
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2013-01-04 01:00:35 +0100

    Bootstrap init files with non secure private and public key

---
+++ bootstrap/init.sh
@@ -0,0 +1,342 @@
+#!/bin/bash -
+#===============================================================================
+# vim: softtabstop=4 shiftwidth=4 expandtab fenc=utf-8 spell spelllang=en
+#===============================================================================
+#
+#          FILE: init.sh
+#
+#   DESCRIPTION: Bootstrap stackops multi-distro installation
+#
+#          BUGS: http://jira.stackops.org/FABULOSO
+#  ORGANIZATION: StackOps (stackops.org)
+#       CREATED: 21/12/2012
+#===============================================================================
+set -o nounset                              # Treat unset variables as an error
+ScriptVersion="1.0"
+ScriptName="init.sh"
+
+usage() {
+    cat << EOT
+
+  Usage :  ${ScriptName} [options] <install-args>
+
+  Options:
+  -h|help       Display this message
+  -v|version    Display script version
+EOT
+}
+
+while getopts ":hvc:" opt
+do
+  case $opt in
+
+    h|help          )  usage; exit 0   ;;
+
+    v|version       )  echo "$0 -- Version $ScriptVersion"; exit 0   ;;
+
+    \?              )  echo "\n  Option does not exist : $OPTARG\n"
+                       usage; exit 1   ;;
+
+  esac
+done
+shift $(($OPTIND-1))
+
+__check_unparsed_options() {
+    shellopts="$1"
+    unparsed_options=$( echo "$shellopts" | grep -E '[-]+[[:alnum:]]' )
+    if [ "x$unparsed_options" != "x" ]; then
+        usage
+        echo
+        echo " * ERROR: options come before install arguments"
+        echo
+        exit 1
+    fi
+}
+
+if [ "$#" -gt 0 ]; then
+    __check_unparsed_options "$*"
+    usage
+    echo
+    echo " * ERROR: Too many arguments."
+    exit 1
+fi
+
+# Root permissions are required to run this script
+if [ $(whoami) != "root" ] ; then
+    echo " * ERROR: Requires root privileges to install. Please re-run this script as root."
+    exit 1
+fi
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_hardware_info
+#   DESCRIPTION:  Discover hardware information
+#-------------------------------------------------------------------------------
+__gather_hardware_info() {
+    if [ -f /proc/cpuinfo ]; then
+        CPU_VENDOR_ID=$(cat /proc/cpuinfo | grep -E 'vendor_id|Processor' | head -n 1 | awk '{print $3}' | cut -d '-' -f1 )
+    else
+        CPU_VENDOR_ID=$( sysctl -n hw.model )
+    fi
+    CPU_VENDOR_ID_L=$( echo $CPU_VENDOR_ID | tr '[:upper:]' '[:lower:]' )
+    CPU_ARCH=$(uname -m 2>/dev/null || uname -p 2>/dev/null || echo "unknown")
+    CPU_ARCH_L=$( echo $CPU_ARCH | tr '[:upper:]' '[:lower:]' )
+
+}
+__gather_hardware_info
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_os_info
+#   DESCRIPTION:  Discover operating system information
+#-------------------------------------------------------------------------------
+__gather_os_info() {
+    OS_NAME=$(uname -s 2>/dev/null)
+    OS_NAME_L=$( echo $OS_NAME | tr '[:upper:]' '[:lower:]' )
+    OS_VERSION=$(uname -r)
+    OS_VERSION_L=$( echo $OS_VERSION | tr '[:upper:]' '[:lower:]' )
+}
+__gather_os_info
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __parse_version_string
+#   DESCRIPTION:  Parse version strings ignoring the revision.
+#                 MAJOR.MINOR.REVISION becomes MAJOR.MINOR
+#-------------------------------------------------------------------------------
+__parse_version_string() {
+    VERSION_STRING="$1"
+    PARSED_VERSION=$(
+        echo $VERSION_STRING |
+        sed -e 's/^/#/' \
+            -e 's/^#[^0-9]*\([0-9][0-9]*\.[0-9][0-9]*\)\(\.[0-9][0-9]*\).*$/\1/' \
+            -e 's/^#[^0-9]*\([0-9][0-9]*\.[0-9][0-9]*\).*$/\1/' \
+            -e 's/^#[^0-9]*\([0-9][0-9]*\).*$/\1/' \
+            -e 's/^#.*$//'
+    )
+    echo $PARSED_VERSION
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_linux_system_info
+#   DESCRIPTION:  Discover Linux system information
+#-------------------------------------------------------------------------------
+__gather_linux_system_info() {
+    DISTRO_NAME=""
+    DISTRO_VERSION=""
+
+    if [ -f /etc/lsb-release ]; then
+        DISTRO_NAME=$(grep DISTRIB_ID /etc/lsb-release | sed -e 's/.*=//')
+        DISTRO_VERSION=$(__parse_version_string $(grep DISTRIB_RELEASE /etc/lsb-release | sed -e 's/.*=//'))
+    fi
+
+    if [ "x$DISTRO_NAME" != "x" -a "x$DISTRO_VERSION" != "x" ]; then
+        # We already have the distribution name and version
+        return
+    fi
+
+    for rsource in $(
+            cd /etc && /bin/ls *[_-]release *[_-]version 2>/dev/null | env -i sort | \
+            sed -e '/^redhat-release$/d' -e '/^lsb-release$/d'; \
+            echo redhat-release lsb-release
+            ); do
+
+        [ -L "/etc/${rsource}" ] && continue        # Don't follow symlinks
+        [ ! -f "/etc/${rsource}" ] && continue      # Does not exist
+
+        n=$(echo ${rsource} | sed -e 's/[_-]release$//' -e 's/[_-]version$//')
+        v=$( __parse_version_string "$( (grep VERSION /etc/${rsource}; cat /etc/${rsource}) | grep '[0-9]' | sed -e 'q' )" )
+        case $(echo ${n} | tr '[:upper:]' '[:lower:]') in
+            redhat )
+                if [ ".$(egrep '(Red Hat Enterprise Linux|CentOS)' /etc/${rsource})" != . ]; then
+                    n="<R>ed <H>at <E>nterprise <L>inux"
+                else
+                    n="<R>ed <H>at <L>inux"
+                fi
+                ;;
+            arch               ) n="Arch"           ;;
+            centos             ) n="CentOS"         ;;
+            debian             ) n="Debian"         ;;
+            ubuntu             ) n="Ubuntu"         ;;
+            fedora             ) n="Fedora"         ;;
+            suse               ) n="SUSE"           ;;
+            mandrake*|mandriva ) n="Mandriva"       ;;
+            gentoo             ) n="Gentoo"         ;;
+            slackware          ) n="Slackware"      ;;
+            turbolinux         ) n="TurboLinux"     ;;
+            unitedlinux        ) n="UnitedLinux"    ;;
+            *                  ) n="${n}"           ;
+        esac
+        DISTRO_NAME=$n
+        DISTRO_VERSION=$v
+        break
+    done
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_sunos_system_info
+#   DESCRIPTION:  Discover SunOS system info
+#-------------------------------------------------------------------------------
+__gather_sunos_system_info() {
+    DISTRO_NAME="Solaris"
+    DISTRO_VERSION=$(
+        echo "${OS_VERSION}" |
+        sed -e 's;^4\.;1.;' \
+            -e 's;^5\.\([0-6]\)[^0-9]*$;2.\1;' \
+            -e 's;^5\.\([0-9][0-9]*\).*;\1;'
+    )
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_bsd_system_info
+#   DESCRIPTION:  Discover OpenBSD, NetBSD and FreeBSD systems information
+#-------------------------------------------------------------------------------
+__gather_bsd_system_info() {
+    DISTRO_NAME=${OS_NAME}
+    DISTRO_VERSION=$(echo "${OS_VERSION}" | sed -e 's;[()];;' -e 's/-.*$//')
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __gather_system_info
+#   DESCRIPTION:  Discover which system and distribution we are running.
+#-------------------------------------------------------------------------------
+__gather_system_info() {
+    case ${OS_NAME_L} in
+        linux )
+            __gather_linux_system_info
+            ;;
+        sunos )
+            __gather_sunos_system_info
+            ;;
+        openbsd|freebsd|netbsd )
+            __gather_bsd_system_info
+            ;;
+        * )
+            echo " * ERROR: $OS_NAME not supported.";
+            exit 1
+            ;;
+    esac
+
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __function_defined
+#   DESCRIPTION:  Checks if a function is defined within this scripts scope
+#    PARAMETERS:  function name
+#       RETURNS:  0 or 1 as in defined or not defined
+#-------------------------------------------------------------------------------
+__function_defined() {
+    FUNC_NAME=$1
+    if [ "${DISTRO_NAME}" = "centos" ]; then
+        if typeset -f $FUNC_NAME &>/dev/null ; then
+            echo " * INFO: Found function $FUNC_NAME"
+            return 0
+        fi
+    elif [ "${DISTRO_NAME}" = "ubuntu" ]; then
+        if $( type ${FUNC_NAME} | grep -q 'shell function' ); then
+            echo " * INFO: Found function $FUNC_NAME"
+            return 0
+        fi
+    # Last resorts try POSIXLY_CORRECT or not
+    elif test -n "${POSIXLY_CORRECT+yes}"; then
+        if typeset -f $FUNC_NAME >/dev/null 2>&1 ; then
+            echo " * INFO: Found function $FUNC_NAME"
+            return 0
+        fi
+    else
+        # Arch linux seems to fall here
+        if $( type ${FUNC_NAME}  >/dev/null 2>&1 ) ; then
+            echo " * INFO: Found function $FUNC_NAME"
+            return 0
+        fi
+    fi
+    echo " * INFO: $FUNC_NAME not found...."
+    return 1
+}
+__gather_system_info
+
+echo " * System Information:"
+echo "     CPU:          ${CPU_VENDOR_ID}"
+echo "     CPU Arch:     ${CPU_ARCH}"
+echo "     OS Name:      ${OS_NAME}"
+echo "     OS Version:   ${OS_VERSION}"
+echo "     Distribution: ${DISTRO_NAME} ${DISTRO_VERSION}"
+
+
+# Simplify version naming on functions
+if [ "x${DISTRO_VERSION}" = "x" ]; then
+    DISTRO_VERSION_NO_DOTS=""
+else
+    DISTRO_VERSION_NO_DOTS="_$(echo $DISTRO_VERSION | tr -d '.')"
+fi
+# Simplify distro name naming on functions
+DISTRO_NAME_L=$(echo $DISTRO_NAME | tr '[:upper:]' '[:lower:]' | sed 's/[^a-zA-Z0-9_ ]//g' | sed -e 's|[:space:]+|_|g')
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  __apt_get_noinput
+#   DESCRIPTION:  (DRY) apt-get install with noinput options
+#-------------------------------------------------------------------------------
+__apt_get_noinput() {
+    apt-get install -y -o DPkg::Options::=--force-confold $@
+}
+
+install_ubuntu_1204() {
+    apt-get update
+    __apt_get_noinput open-ssh
+}
+
+conf_ubuntu_1204() {
+    adduser stackops sudo
+    echo 'stackops:stackops'|chpasswd
+    su stackops -c 'rm -fR ~/.ssh'
+    su stackops -c 'mkdir ~/.ssh'
+    su stackops -c 'chmod 700 ~/.ssh'
+    su stackops -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC07jNnA1g7O7H7Ka+tbuQyiMn12IjtbGY+8RJrUU11jjlM3llZhJhyBgoNP864cOL37j0uVE+SxXfmxwY0glupX0KvgvS8dd9v0T0R04giS/eM4b1CGHAg/EquklA/WGQ/LHJtaQf8hUdjTo7EshY8k7c0LvixBC9dnaYxSg5bkpxDCIoZ9Z/eYkoJxyhw2/cxc/hxvprLsNC1uBCKCNlqhDF9+Qm+rTzeHfLmVJCmcjnrIERce0dqbxcI+e7sDGPS/kAYZpNg5rhEDOQbw8qly8vxNGM8vdMyKIokzatBWsd2NwqoFI4Kv59I2WvsTkpUE3yl+uyaXL42h+WEDuF1 contactus@stackops.com" > ~/.ssh/authorized_keys'
+    su stackops -c 'chmod 600 ~/.ssh/authorized_keys'
+    sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+    sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+    sed -i 's/%sudo.*ALL=(ALL:ALL) ALL/%sudo ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
+    echo "\nUseDNS no" >> /etc/ssh/sshd_config
+}
+
+post_install_ubuntu_1204() {
+    service ssh restart
+}
+
+DEPS_INSTALL_FUNC="install_${DISTRO_NAME_L}${DISTRO_VERSION_NO_DOTS}"
+CONFIG_FUNC="conf_${DISTRO_NAME_L}${DISTRO_VERSION_NO_DOTS}"
+POST_INSTALL_FUNC="post_install_${DISTRO_NAME_L}${DISTRO_VERSION_NO_DOTS}"
+
+# Install dependencies
+echo " * Running ${DEPS_INSTALL_FUNC}()"
+$DEPS_INSTALL_FUNC
+if [ $? -ne 0 ]; then
+    echo " * Failed to run ${DEPS_INSTALL_FUNC}()!!!"
+    exit 1
+fi
+
+# Configure basic stuff
+$CONFIG_FUNC
+if [ $? -ne 0 ]; then
+    echo " * Failed to run ${CONFIG_FUNC}()!!!"
+    exit 1
+fi
+
+if [ "$POST_INSTALL_FUNC" != "null" ]; then
+    echo " * Running ${POST_INSTALL_FUNC}()"
+    $POST_INSTALL_FUNC
+    if [ $? -ne 0 ]; then
+        echo " * Failed to run ${POST_INSTALL_FUNC}()!!!"
+        exit 1
+    fi
+fi
+
+# Done!
+echo " * Basic stuff installed!"
+exit 0
